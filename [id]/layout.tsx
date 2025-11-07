9b3053fc3d634caa04ab5ef3cb038e22cd29ab07
Aman Zaid <Aman Zaid>
2025-11-08 02:42:05 +0530

    feat(editor): add editor layout

---
+++ [id]/layout.tsx
@@ -0,0 +1,7 @@
+export default async function EditorLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
--- [id]/page.tsx
+++ [id]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState, Suspense } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import dynamic from "next/dynamic";
 import { api } from "../services/api";
